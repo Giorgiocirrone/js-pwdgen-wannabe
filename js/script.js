b0c5b6f512d487de3f37f8bff68862861afc3291
Giorgiocirrone <cirronegiorgio@gmail.com>
2024-07-30 17:34:59 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log('js ok ')
+/*
+
+1-chiedo all'utente il suo nome 
+2-chiedo all'utente il suo cognome 
+3-chiedo all'utente il suo colore preferito 
+4-salvo i suoi dati 
+
+
+
+
+
+
+*/
+
+//DICHIARAZIONE VARIABILI 
+
+
+const myDiv = document.getElementById('my-div');
+
+const firstName = window.prompt('Qual\è il tuo nome ? ');
+
+
+const secondName = window.prompt('Qual\è il tuo cognome ? ');
+
+
+const colore = window.prompt('Qual\è il tuo colore preferito ? ')
+
+const number = ('21')
+
+
+const fullName = firstName + secondName;
+
+const passk = fullName + number
+
+
+
+myDiv.innerText = Ciao
+
